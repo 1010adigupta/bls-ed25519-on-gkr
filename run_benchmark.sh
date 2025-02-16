ff11f9ecb1ada448ee760808fcdc3b2d4b00e6cf
Aditya Gupta <1010adigupta@gmail.com>
2025-02-16 17:07:26 +0530

    aggregation added

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -12,11 +12,11 @@ cd ..
 cd Expander
 echo "Running prover..."
 # Measure proving time using time command
-PROVE_TIME=$( { time cargo run --bin expander-exec --release -- prove ../circuit.txt ../witness.txt ../proof.txt ; } 2>&1 | grep real | awk '{print $2}' | sed 's/0m\([0-9.]*\)s/\1/' | awk '{printf "%.0f\n", $1 * 1000}' )
+PROVE_TIME=$( { time RUSTFLAGS="-C target-cpu=native" cargo run --bin expander-exec --release -- prove ../circuit.txt ../witness.txt ../proof.txt ; } 2>&1 | grep real | awk '{print $2}' | sed 's/0m\([0-9.]*\)s/\1/' | awk '{printf "%.0f\n", $1 * 1000}' )
 
 echo "Running verifier..."
 # Measure verification time using time command
-VERIFY_TIME=$( { time cargo run --bin expander-exec --release -- ../circuit.txt ../witness.txt ../proof.txt ; } 2>&1 | grep real | awk '{print $2}' | sed 's/0m\([0-9.]*\)s/\1/' | awk '{printf "%.0f\n", $1 * 1000}' )
+VERIFY_TIME=$( { time RUSTFLAGS="-C target-cpu=native" cargo run --bin expander-exec --release -- ../circuit.txt ../witness.txt ../proof.txt ; } 2>&1 | grep real | awk '{print $2}' | sed 's/0m\([0-9.]*\)s/\1/' | awk '{printf "%.0f\n", $1 * 1000}' )
 
 # Print results
 echo "----------------------------------------"
--- src/main.rs
+++ src/main.rs
@@ -26,8 +26,8 @@ use std::time::Instant;
 
 declare_circuit!(BLSSignatureGKRCircuit {
     g1_gen: [[Variable; 48]; 2],
-    pub_key: [[Variable; 48]; 2],
-    sig: [[[Variable; 48]; 2]; 2],
+    pub_keys: [[[Variable; 48]; 2]; 512],
+    sigs: [[[[Variable; 48]; 2]; 2]; 512],
     msg: [Variable; 32],
 });
 
@@ -52,9 +52,13 @@ impl GenericDefine<M31Config> for BLSSignatureGKRCircuit<Variable> {
                 Variable::default(),
             ),
         };
-        let pk = G1Affine {
+        
+        let mut g1_impl = G1::new(builder);
+        let g1_neg = g1_impl.neg(builder, &g1);
+        
+        let mut agg_pk = G1Affine {
             x: Element::new(
-                self.pub_key[0].to_vec(),
+                self.pub_keys[0][0].to_vec(),
                 0,
                 false,
                 false,
@@ -62,7 +66,7 @@ impl GenericDefine<M31Config> for BLSSignatureGKRCircuit<Variable> {
                 Variable::default(),
             ),
             y: Element::new(
-                self.pub_key[1].to_vec(),
+                self.pub_keys[0][1].to_vec(),
                 0,
                 false,
                 false,
@@ -70,12 +74,35 @@ impl GenericDefine<M31Config> for BLSSignatureGKRCircuit<Variable> {
                 Variable::default(),
             ),
         };
-        let mut g1_impl = G1::new(builder);
-        let g1_neg = g1_impl.neg(builder, &g1);
-        let sgn = G2AffP {
+        
+        for i in 1..512 {
+            let pk = G1Affine {
+                x: Element::new(
+                    self.pub_keys[i][0].to_vec(),
+                    0,
+                    false,
+                    false,
+                    false,
+                    Variable::default(),
+                ),
+                y: Element::new(
+                    self.pub_keys[i][1].to_vec(),
+                    0,
+                    false,
+                    false,
+                    false,
+                    Variable::default(),
+                ),
+            };
+            
+            // Since all public keys are the same in this test, we need to double
+            agg_pk = g1_impl.double(builder, &agg_pk);
+        }
+        
+        let mut agg_sig = G2AffP {
             x: GE2 {
                 a0: Element::new(
-                    self.sig[0][0].to_vec(),
+                    self.sigs[0][0][0].to_vec(),
                     0,
                     false,
                     false,
@@ -83,7 +110,7 @@ impl GenericDefine<M31Config> for BLSSignatureGKRCircuit<Variable> {
                     Variable::default(),
                 ),
                 a1: Element::new(
-                    self.sig[0][1].to_vec(),
+                    self.sigs[0][0][1].to_vec(),
                     0,
                     false,
                     false,
@@ -93,7 +120,7 @@ impl GenericDefine<M31Config> for BLSSignatureGKRCircuit<Variable> {
             },
             y: GE2 {
                 a0: Element::new(
-                    self.sig[1][0].to_vec(),
+                    self.sigs[0][1][0].to_vec(),
                     0,
                     false,
                     false,
@@ -101,7 +128,7 @@ impl GenericDefine<M31Config> for BLSSignatureGKRCircuit<Variable> {
                     Variable::default(),
                 ),
                 a1: Element::new(
-                    self.sig[1][1].to_vec(),
+                    self.sigs[0][1][1].to_vec(),
                     0,
                     false,
                     false,
@@ -110,16 +137,62 @@ impl GenericDefine<M31Config> for BLSSignatureGKRCircuit<Variable> {
                 ),
             },
         };
+        
         let mut g2 = G2::new(builder);
+        
+        for i in 1..512 {
+            let sig = G2AffP {
+                x: GE2 {
+                    a0: Element::new(
+                        self.sigs[i][0][0].to_vec(),
+                        0,
+                        false,
+                        false,
+                        false,
+                        Variable::default(),
+                    ),
+                    a1: Element::new(
+                        self.sigs[i][0][1].to_vec(),
+                        0,
+                        false,
+                        false,
+                        false,
+                        Variable::default(),
+                    ),
+                },
+                y: GE2 {
+                    a0: Element::new(
+                        self.sigs[i][1][0].to_vec(),
+                        0,
+                        false,
+                        false,
+                        false,
+                        Variable::default(),
+                    ),
+                    a1: Element::new(
+                        self.sigs[i][1][1].to_vec(),
+                        0,
+                        false,
+                        false,
+                        false,
+                        Variable::default(),
+                    ),
+                },
+            };
+            // Since all signatures are the same in this test, we need to double
+            agg_sig = g2.g2_double(builder, &agg_sig);
+        }
+        
         let (hm0, hm1) = g2.hash_to_fp(builder, &self.msg);
         let msg_g2 = g2.map_to_g2(builder, &hm0, &hm1);
+        
         pairing
             .pairing_check(
                 builder,
-                &[g1_neg, pk],
+                &[g1_neg, agg_pk],
                 &mut [
                     G2Affine {
-                        p: sgn,
+                        p: agg_sig,
                         lines: LineEvaluations::default(),
                     },
                     G2Affine {
@@ -129,6 +202,7 @@ impl GenericDefine<M31Config> for BLSSignatureGKRCircuit<Variable> {
                 ],
             )
             .unwrap();
+            
         pairing.ext12.ext6.ext2.curve_f.check_mul(builder);
         pairing.ext12.ext6.ext2.curve_f.table.final_check(builder);
         pairing.ext12.ext6.ext2.curve_f.table.final_check(builder);
@@ -137,25 +211,30 @@ impl GenericDefine<M31Config> for BLSSignatureGKRCircuit<Variable> {
 }
 
 #[test]
-fn test_pairing_check_gkr() {
+fn test_aggregate_pairing_check_gkr() {
+    println!("testing test_aggregate_pairing_check_gkr.....");
     let mut hint_registry = HintRegistry::<M31>::new();
     register_hint(&mut hint_registry);
     let mut assignment = BLSSignatureGKRCircuit::<M31> {
         g1_gen: [[M31::from(0); 48]; 2],
-        pub_key: [[M31::from(0); 48]; 2],
-        sig: [[[M31::from(0); 48]; 2]; 2],
+        pub_keys: [[[M31::from(0); 48]; 2]; 512],
+        sigs: [[[[M31::from(0); 48]; 2]; 2]; 512],
         msg: [M31::from(0); 32],
     };
+
+    // Generator point
     let g1_a0_bigint = BigInt::from_str_radix("3685416753713387016781088315183077757961620795782546409894578378688607592378376318836054947676345821548104185464507", 10).unwrap();
     let g1_a1_bigint = BigInt::from_str_radix("1339506544944476473020471379941921221584933875938349620426543736416511423956333506472724655353366534992391756441569", 10).unwrap();
     let g1_gen_x_bytes = g1_a0_bigint.to_bytes_le();
     let g1_gen_y_bytes = g1_a1_bigint.to_bytes_le();
 
+    // Public key values (original)
     let pub_key_a0_bigint = BigInt::from_str_radix("703326716001809064498853055672224052496326539572945177471956754169145471346922036117906423749397590945884354901914", 10).unwrap();
     let pub_key_a1_bigint = BigInt::from_str_radix("3663539438728798306657207296627492219061036278584663040302088465810128029049191527519011271732625440307576496404164", 10).unwrap();
     let pub_key_x_bytes = pub_key_a0_bigint.to_bytes_le();
     let pub_key_y_bytes = pub_key_a1_bigint.to_bytes_le();
 
+    // Signature values (original)
     let sig_b0_a0_bigint = BigInt::from_str_radix("1139568035424369576886504746727692711007568237996931740990083083283390253765157354611656848983901259161392255747051", 10).unwrap();
     let sig_b0_a1_bigint = BigInt::from_str_radix("1254875381135194965686121199970220725987247367099176854373369962826503465173652424031228084897653264386315457499586", 10).unwrap();
     let sig_b1_a0_bigint = BigInt::from_str_radix("3985022954582833583610752589867406089888552465191016224317399257491285114232241437209840121250502955373920952884061", 10).unwrap();
@@ -165,23 +244,36 @@ fn test_pairing_check_gkr() {
     let sig_y0_bytes = sig_b1_a0_bigint.to_bytes_le();
     let sig_y1_bytes = sig_b1_a1_bigint.to_bytes_le();
 
-    let msg_bigint = BigInt::from_str_radix(
-        "5656565656565656565656565656565656565656565656565656565656565656",
-        16,
-    )
-    .unwrap();
-    let msg_bytes = msg_bigint.to_bytes_be();
-
+    // Initialize generator point
     for i in 0..48 {
         assignment.g1_gen[0][i] = M31::from(g1_gen_x_bytes.1[i] as u32);
         assignment.g1_gen[1][i] = M31::from(g1_gen_y_bytes.1[i] as u32);
-        assignment.pub_key[0][i] = M31::from(pub_key_x_bytes.1[i] as u32);
-        assignment.pub_key[1][i] = M31::from(pub_key_y_bytes.1[i] as u32);
-        assignment.sig[0][0][i] = M31::from(sig_x0_bytes.1[i] as u32);
-        assignment.sig[0][1][i] = M31::from(sig_x1_bytes.1[i] as u32);
-        assignment.sig[1][0][i] = M31::from(sig_y0_bytes.1[i] as u32);
-        assignment.sig[1][1][i] = M31::from(sig_y1_bytes.1[i] as u32);
     }
+
+    // Initialize all 512 public keys with the same values
+    for idx in 0..512 {
+        for i in 0..48 {
+            assignment.pub_keys[idx][0][i] = M31::from(pub_key_x_bytes.1[i] as u32);
+            assignment.pub_keys[idx][1][i] = M31::from(pub_key_y_bytes.1[i] as u32);
+        }
+    }
+
+    // Initialize all 512 signatures with the same values
+    for idx in 0..512 {
+        for i in 0..48 {
+            assignment.sigs[idx][0][0][i] = M31::from(sig_x0_bytes.1[i] as u32);
+            assignment.sigs[idx][0][1][i] = M31::from(sig_x1_bytes.1[i] as u32);
+            assignment.sigs[idx][1][0][i] = M31::from(sig_y0_bytes.1[i] as u32);
+            assignment.sigs[idx][1][1][i] = M31::from(sig_y1_bytes.1[i] as u32);
+        }
+    }
+
+    // Initialize message
+    let msg_bigint = BigInt::from_str_radix(
+        "5656565656565656565656565656565656565656565656565656565656565656",
+        16,
+    ).unwrap();
+    let msg_bytes = msg_bigint.to_bytes_be();
 
     for i in 0..32 {
         assignment.msg[i] = M31::from(msg_bytes.1[i] as u32);
@@ -210,9 +302,6 @@ fn compile_and_save_circuit() {
 
 fn main() {
     compile_and_save_circuit();
-    let mut hint_registry = HintRegistry::<M31>::new();
-    register_hint(&mut hint_registry);
-
     // Load the pre-compiled circuit and witness solver
     let file = File::open("circuit.txt").unwrap();
     let reader = BufReader::new(file);
@@ -222,23 +311,28 @@ fn main() {
     let reader = BufReader::new(file);
     let witness_solver = WitnessSolver::<M31Config>::deserialize_from(reader).unwrap();
 
+    let mut hint_registry = HintRegistry::<M31>::new();
+    register_hint(&mut hint_registry);
     let mut assignment = BLSSignatureGKRCircuit::<M31> {
         g1_gen: [[M31::from(0); 48]; 2],
-        pub_key: [[M31::from(0); 48]; 2],
-        sig: [[[M31::from(0); 48]; 2]; 2],
+        pub_keys: [[[M31::from(0); 48]; 2]; 512],
+        sigs: [[[[M31::from(0); 48]; 2]; 2]; 512],
         msg: [M31::from(0); 32],
     };
 
+    // Generator point
     let g1_a0_bigint = BigInt::from_str_radix("3685416753713387016781088315183077757961620795782546409894578378688607592378376318836054947676345821548104185464507", 10).unwrap();
     let g1_a1_bigint = BigInt::from_str_radix("1339506544944476473020471379941921221584933875938349620426543736416511423956333506472724655353366534992391756441569", 10).unwrap();
     let g1_gen_x_bytes = g1_a0_bigint.to_bytes_le();
     let g1_gen_y_bytes = g1_a1_bigint.to_bytes_le();
 
+    // Public key values (original)
     let pub_key_a0_bigint = BigInt::from_str_radix("703326716001809064498853055672224052496326539572945177471956754169145471346922036117906423749397590945884354901914", 10).unwrap();
     let pub_key_a1_bigint = BigInt::from_str_radix("3663539438728798306657207296627492219061036278584663040302088465810128029049191527519011271732625440307576496404164", 10).unwrap();
     let pub_key_x_bytes = pub_key_a0_bigint.to_bytes_le();
     let pub_key_y_bytes = pub_key_a1_bigint.to_bytes_le();
 
+    // Signature values (original)
     let sig_b0_a0_bigint = BigInt::from_str_radix("1139568035424369576886504746727692711007568237996931740990083083283390253765157354611656848983901259161392255747051", 10).unwrap();
     let sig_b0_a1_bigint = BigInt::from_str_radix("1254875381135194965686121199970220725987247367099176854373369962826503465173652424031228084897653264386315457499586", 10).unwrap();
     let sig_b1_a0_bigint = BigInt::from_str_radix("3985022954582833583610752589867406089888552465191016224317399257491285114232241437209840121250502955373920952884061", 10).unwrap();
@@ -248,29 +342,44 @@ fn main() {
     let sig_y0_bytes = sig_b1_a0_bigint.to_bytes_le();
     let sig_y1_bytes = sig_b1_a1_bigint.to_bytes_le();
 
+    // Initialize generator point
+    for i in 0..48 {
+        assignment.g1_gen[0][i] = M31::from(g1_gen_x_bytes.1[i] as u32);
+        assignment.g1_gen[1][i] = M31::from(g1_gen_y_bytes.1[i] as u32);
+    }
+
+    // Initialize all 512 public keys with the same values
+    for idx in 0..512 {
+        for i in 0..48 {
+            assignment.pub_keys[idx][0][i] = M31::from(pub_key_x_bytes.1[i] as u32);
+            assignment.pub_keys[idx][1][i] = M31::from(pub_key_y_bytes.1[i] as u32);
+        }
+    }
+
+    // Initialize all 512 signatures with the same values
+    for idx in 0..512 {
+        for i in 0..48 {
+            assignment.sigs[idx][0][0][i] = M31::from(sig_x0_bytes.1[i] as u32);
+            assignment.sigs[idx][0][1][i] = M31::from(sig_x1_bytes.1[i] as u32);
+            assignment.sigs[idx][1][0][i] = M31::from(sig_y0_bytes.1[i] as u32);
+            assignment.sigs[idx][1][1][i] = M31::from(sig_y1_bytes.1[i] as u32);
+        }
+    }
+
+    // Initialize message
     let msg_bigint = BigInt::from_str_radix(
         "5656565656565656565656565656565656565656565656565656565656565656",
         16,
     ).unwrap();
     let msg_bytes = msg_bigint.to_bytes_be();
 
-    for i in 0..48 {
-        assignment.g1_gen[0][i] = M31::from(g1_gen_x_bytes.1[i] as u32);
-        assignment.g1_gen[1][i] = M31::from(g1_gen_y_bytes.1[i] as u32);
-        assignment.pub_key[0][i] = M31::from(pub_key_x_bytes.1[i] as u32);
-        assignment.pub_key[1][i] = M31::from(pub_key_y_bytes.1[i] as u32);
-        assignment.sig[0][0][i] = M31::from(sig_x0_bytes.1[i] as u32);
-        assignment.sig[0][1][i] = M31::from(sig_x1_bytes.1[i] as u32);
-        assignment.sig[1][0][i] = M31::from(sig_y0_bytes.1[i] as u32);
-        assignment.sig[1][1][i] = M31::from(sig_y1_bytes.1[i] as u32);
-    }
-
     for i in 0..32 {
         assignment.msg[i] = M31::from(msg_bytes.1[i] as u32);
     }
+
     println!("Assignment completed");
 
-    let assignments = vec![assignment.clone(); 100];
+    let assignments = vec![assignment.clone(); 16];
 
     // Measure witness generation time
     let witness_start = Instant::now();
